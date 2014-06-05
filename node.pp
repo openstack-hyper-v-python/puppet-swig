22340417be9a5d94308ee84243338e98aaa5bea4
paranoidgabe <gloewen@crimson.ua.edu>
2014-06-05 12:04:58 -0700

    added node.pp

---
+++ node.pp
@@ -0,0 +1,18 @@
+node python-build-server {
+    include init
+    include windows_git
+    include jenkins
+    jenkins::plugin {
+        "git" : ;
+        "github" : ;
+    }
+    include jenkins_job_builder
+    include nasm
+    include mingw # this needs to include gendef and pthreads!
+    include wdk
+    include dotnet35
+    include visualcplusplus2008
+    include platformsdk
+    include swig
+    include mysql
+}
